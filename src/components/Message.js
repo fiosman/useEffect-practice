4a5e2ba5ccc58d401fe8e2460cf6b607ce4d3418
fiosman <fares.osman@outlook.com>
2021-12-26 23:24:07 -0500

    Updates message size class in use effect

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Message({ size }) {
+  const [messageSize, setMessageSize] = useState();
+
   useEffect(() => {
-    console.log("Message", size);
+    switch (size) {
+      case "s":
+        setMessageSize("small");
+        break;
+      case "m":
+        setMessageSize("medium");
+        break;
+      case "l":
+        setMessageSize("large");
+        break;
+      case "xl":
+        setMessageSize("xlarge");
+        break;
+      default:
+        setMessageSize("small");
+        break;
+    }
   }, [size]);
-  return <div className="message medium">(Oh my! Your bird is naked!)</div>;
+  return <div className={`message ${messageSize}`}>(Oh my! Your bird is naked!)</div>;
 }
 
 export default Message;
